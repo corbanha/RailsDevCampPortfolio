5b5b875687f58fe56b2242d214fcc42d8b5dc3d6
Corban Anderson <corban@corbanha.com>
2020-05-18 22:13:29 -0600

    Added edit and update Portfolio paths

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -20,6 +20,23 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio was created!' }
+        format.json { render portfolios_path, status: :ok, location: portfolios_path }
+      else
+        format.html { render :edit, danger: 'There was an error updating your portfolio' }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def portfolio_params
