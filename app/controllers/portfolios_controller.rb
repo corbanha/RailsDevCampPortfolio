32263fd2c629764118f150d428b17cd56ea5b924
Corban Anderson <corban@corbanha.com>
2020-05-18 21:58:07 -0600

    Added new and create Portfolio controller routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,8 +4,25 @@ class PortfoliosController < ApplicationController
     @portfolios = Portfolio.all
   end
 
-  def create
-
+  def new
+    @portfolio = Portfolio.new
   end
 
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, success: "Your portfolio was created!" }
+      else
+        format.html { render :new, danger: "There was an error creating your portfolio" }
+      end
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, :thumbnail, :main_image)
+  end
 end
