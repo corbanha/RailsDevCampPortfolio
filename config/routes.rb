e04c0f3eb8baaa95e09e0cfa39c858468c5084c1
Corban Anderson <corban@corbanha.com>
2020-05-18 21:22:37 -0600

    Added seeds file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
   resources :portfolios
-  root to: 'pages/home'
+
+  root to: 'pages#home'
   get 'pages/about'
   get 'pages/contact'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(title: "Epic Blog ##{blog}", body: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.')
+end
+
+9.times do |portfolio|
+  Portfolio.create!(title: "Epic Portfolio ##{portfolio}",
+                    subtitle: "Here is the subtitle for porfolio ##{portfolio}",
+                    body: 'Here is some body text',
+                    main_image: 'https://picsum.photos/600/400',
+                    thumbnail: 'https://picsum.photos/350/200')
+end
+
+5.times do |skill|
+  Skill.create!(title: "Here is skill ##{skill}", percent_utilized: skill * 8)
+end
