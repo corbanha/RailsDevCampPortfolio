5f59ca455152b1efe50532d3821a1a7c2eab69b7
Corban Anderson <corban@corbanha.com>
2020-05-19 21:52:23 -0600

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :portfolios
-
   root to: 'pages#home'
-  get 'pages/about'
-  get 'pages/contact'
+
+  pages = %w[about contact]
+  pages.each do |page|
+    get page, to: "pages##{page}"
+  end
 
   resources :blogs
+
+  # singlize_methods = %w[show, update, destroy]
+  # resources :portfolios, except: %i[show, update, destroy]
+  # singlize_methods.each do |method|
+  #   get 'portfolio/:id', to: "portfolios##{method}"
+  # end
+
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
 end
