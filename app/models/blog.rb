2a95b25759ea01fdc63f4e5fe49609a7530c1df0
Corban Anderson <corban@corbanha.com>
2020-05-21 20:51:48 -0600

    Added draft/published enum to blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,9 @@
 class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  enum status: {
+    draft: 0,       # will automatically add draft! and draft?, published! and published? methods
+    published: 1
+  }
 end
