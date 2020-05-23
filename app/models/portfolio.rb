7bb7563f158640cdff1eef06a0f35fa965d82a78
Corban Anderson <corban@corbanha.com>
2020-05-23 15:48:13 -0600

    Implemented image defaults for portfolio from the model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,18 @@
 class Portfolio < ApplicationRecord
-  validates_presence_of :title, :body, :main_image, :thumbnail_change
+  validates_presence_of :title, :body, :main_image, :thumbnail
+
+  after_initialize :set_defaults
+
+  scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 
   def self.angular
     where(subtitle: 'Angular')
   end
 
-  scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
+  private
+
+  def set_defaults
+    self.main_image ||= 'https://picsum.photos/300/400'
+    self.thumbnail ||= 'https://picsum.photos/350/200'
+  end
 end
