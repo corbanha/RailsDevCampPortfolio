a0efe052837d9c9d8d78a88dd124ec0e7b8685e0
Corban Anderson <corban@corbanha.com>
2020-05-25 21:40:55 -0600

    Added session tracker for user source website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
+
+  before_action :set_source
+
+  protected
+
+  def set_source
+    session[:source] = params[:s] if params[:s]
+  end
 end
