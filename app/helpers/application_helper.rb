a720575543ed5c9ed970620b126f06e11f4c146c
Corban Anderson <corban@corbanha.com>
2020-05-30 15:19:43 -0600

    Implemented view helper function for log in links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def login_helper
+    if current_user.is_a?(User)
+      (link_to 'Log Out', destroy_user_session_path, method: :delete) + '<br />'.html_safe +
+      (link_to 'Edit Account', edit_user_registration_path)
+    else
+      (link_to 'Create a Free Account!', new_user_registration_path) + '<br />'.html_safe +
+      (link_to 'Log in', new_user_session_path)
+    end
+  end
 end
