e9fde130b577079bfd79b1f9af327b38be9f9e0a
Corban Anderson <corban@corbanha.com>
2020-06-05 15:21:23 -0600

    Debugging things

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -10,4 +10,12 @@ class Blog < ApplicationRecord
     draft: 0,       # will automatically add draft! and draft?, published! and published? methods
     published: 1
   }
+
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
+  end
 end
